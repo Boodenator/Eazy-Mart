abb683f35d0f5acd4d8b35f7d05186e92646c235
Boodenator <boode1000@gmail.com>
2025-01-08 14:49:26 +0200

    added counts to cart button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { CartContext } from '../context/CartContext';
 import './Header.css';
 const Header = () => {
+  const { cart } = useContext(CartContext);
+
   return (
     <header>
       <nav className='headerNav'>
         <button className="headerButton"><Link to="/">Home</Link></button>
-        <button className="headerButton"><Link to="/cart">Cart</Link></button>
+        <button className="headerButton"><Link to="/cart">Cart</Link>
+        {cart.length > 0 && <span className="cartCount">({cart.length})</span>} {/* Display cart item count */}
+        </button>
         <button className="headerButton"><Link to="/SignIn">Sign In</Link></button>
       </nav>
     </header>
